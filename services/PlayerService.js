fdb110f2ecf37bfad89a92861618ee5e8a427847
SONEPAR\kjacobson <kraig.jacobson@vallen.com>
2017-05-10 21:39:10 -0400

    Now forces state change when clicking travel button

--- services/PlayerService.js
+++ services/PlayerService.js
@@ -1,20 +1,18 @@
-app.service('PlayerService', function($rootScope, ShipService, UniverseService, DataService){
+app.service('PlayerService', function($rootScope, $state, ShipService, UniverseService, DataService){
 
     var self = this;
 
     this.travel = function () {
 
-        var distance = ShipService.getDistance();
-        DataService.stats.distanceLeft -= distance;
-        DataService.stats.distanceTraveled += distance;
-        $rootScope.$broadcast('getLog', { log: "You have traveled " + distance + " light years." });
-        event = UniverseService.event();
+        $state.go('log').then(function () {
 
-    };
+            var distance = ShipService.getDistance();
+            DataService.stats.distanceLeft -= distance;
+            DataService.stats.distanceTraveled += distance;
+            $rootScope.$broadcast('getLog', { log: "You have traveled " + distance + " light years." });
+            event = UniverseService.event();
 
-    this.updateStat = function (stat, amt) {
-
-        stat[stat] += amt;
+        });
 
     };
 
